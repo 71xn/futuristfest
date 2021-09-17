a11f3ddec1d5012fae388659268d7ef3aad4b0e0
Finn Lestrange <44287141+71xn@users.noreply.github.com>
2021-09-17 19:23:23 +0100

    created react template for project

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import Navigation from './components/navigation';
+
+function App() {
+  return (
+    <div>
+      <Navigation />
+    <div className="App">
+      <header>
+        This it a test header
+      </header>
+      This is another test - not in header
+    </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/navigation.js
@@ -0,0 +1,34 @@
+import { Navbar, Button, Nav, Container } from "react-bootstrap";
+import logo from "../logo.png";
+import "../styles/navigation.css";
+
+const onButtonClick = () => {
+	window.open("https://github.com/71xn");
+};
+
+const Navigation = () => {
+	return (
+		<Navbar bg="dark" variant="dark" fixed="top" expand="lg">
+			<Container>
+				<Navbar.Brand href="/">
+					<img
+						alt=""
+						src={logo}
+						width="30"
+						height="30"
+						className="d-inline-block align-top"
+					/>{" "}
+					FutureFest 2021
+				</Navbar.Brand>
+				<Navbar.Toggle />
+				<Navbar.Collapse className="justify-content-end">
+					<Button variant="outline-success" onClick={onButtonClick}>
+						My GitHub!
+					</Button>
+				</Navbar.Collapse>
+			</Container>
+		</Navbar>
+	);
+};
+
+export default Navigation;
