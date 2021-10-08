23fe6a65a25d9564a8835b5bdde92c3e95f203f8
Finn Lestrange <44287141+71xn@users.noreply.github.com>
2021-10-08 10:12:15 +0100

    updated api and results

--- src/components/ScoreBreakdown.js
+++ src/components/ScoreBreakdown.js
@@ -2,33 +2,63 @@ import React from "react";
 import { Alert } from "react-bootstrap";
 import "../styles/ScoreBreakdown.css";
 
+const colorSuggestion = (text) => {
+  if (text === "Above average") {
+    return <p style={{ color: "red" }}>{text}</p>;
+  } else if (text === "Average") {
+    return <p style={{ color: "yellow" }}>{text}</p>;
+  } else if (text === "Below average") {
+    return <p style={{ color: "green" }}>{text}</p>;
+  }
+};
+
 export default function ScoreBreakdown(props) {
-	// Suggestions
-	// props.food
-	// props.transport
-	// props.fights
-	// props.house
-	return (
-		<div>
-			<Alert show={true} variant="info" className="Alert">
-				<Alert.Heading className="AlertHeading">
-					General Information
-				</Alert.Heading>
-				<hr />
-				<p>
-					Your score is a value between <code>0 and 100</code>, where a value of
-					<code> 50</code> represents the average person in your country, less
-					than <code>50</code> is better than average and more than{" "}
-					<code>50</code> is worse than average.
-				</p>
-				<p>
-					The following section is a breakdown of your score with suggestions
-					for being more environmentally conscious.
-				</p>
-			</Alert>
-			<br />
-			<p className="title">This is your score breakdown</p>
-			<hr />
-		</div>
-	);
+  // Suggestions
+  // props.suggestions
+  // props.food
+  // props.transport
+  // props.fights
+  // props.house
+  return (
+    <div>
+      <Alert show={true} variant="info" className="Alert">
+        <Alert.Heading className="AlertHeading">
+          General Information
+        </Alert.Heading>
+        <hr />
+        <p>
+          Your score is a value between <code>0 and 100</code>, where a value of
+          <code> 50</code> represents the average person in your country, less
+          than <code>50</code> is better than average and more than{" "}
+          <code>50</code> is worse than average.
+        </p>
+        <p>
+          The following section is a breakdown of your score with suggestions
+          for being more environmentally conscious.
+        </p>
+        <br />
+        <p>
+          <b>Please Note: </b> Your score may not be a perfect representation of
+          your carbon footprint, especially if you live in a less economically
+          developed country due to the lack of data collected about said
+          countries.
+        </p>
+      </Alert>
+      <br />
+      <p className="title">This is your score breakdown</p>
+      <hr />
+      Your food carbon emissions are: {colorSuggestion(props.suggestions[0])}
+      <br />
+      Your home's carbon emissions are: {colorSuggestion(props.suggestions[1])}
+      <br />
+      Your transport carbon emissions are:{" "}
+      {colorSuggestion(props.suggestions[2])}
+      <br />
+      The carbon emissions for your energy are:{" "}
+      {colorSuggestion(props.suggestions[3])}
+      <br />
+      Your carbon emissions from your flights are:{" "}
+      {colorSuggestion(props.suggestions[4])}
+    </div>
+  );
 }
--- src/components/navigation.js
+++ src/components/navigation.js
@@ -3,37 +3,37 @@ import logo from "../logo.png";
 import "../styles/navigation.css";
 
 const onButtonClick = () => {
-	window.open("https://github.com/71xn");
+  window.open("https://github.com/71xn");
 };
 
 const Navigation = () => {
-	return (
-		<Navbar bg="dark" variant="dark" fixed="top">
-			<Container>
-				<Navbar.Brand href="/">
-					<img
-						alt=""
-						src={logo}
-						width="30"
-						height="30"
-						className="d-inline-block align-top"
-					/>{" "}
-					FuturistFest 2021 ➡ Environmental Issue
-				</Navbar.Brand>
-				<Nav className="me-auto">
-					<Nav.Link href="/">Home</Nav.Link>
-					<Nav.Link href="/about">About</Nav.Link>
-					<Nav.Link href="/contact">Contact</Nav.Link>
-				</Nav>
-				<Navbar.Toggle />
-				<Navbar.Collapse className="justify-content-end">
-					<Button variant="outline-success" onClick={onButtonClick}>
-						My GitHub!
-					</Button>
-				</Navbar.Collapse>
-			</Container>
-		</Navbar>
-	);
+  return (
+    <Navbar bg="dark" variant="dark" fixed="top">
+      <Container>
+        <Navbar.Brand href="/">
+          <img
+            alt=""
+            src={logo}
+            width="30"
+            height="30"
+            className="d-inline-block align-top"
+          />{" "}
+          FuturistFest 2021 ➡ Carbon Footprint Calculator
+        </Navbar.Brand>
+        <Nav className="me-auto">
+          <Nav.Link href="/">Home</Nav.Link>
+          <Nav.Link href="/about">About</Nav.Link>
+          <Nav.Link href="/contact">Contact</Nav.Link>
+        </Nav>
+        <Navbar.Toggle />
+        <Navbar.Collapse className="justify-content-end">
+          <Button variant="outline-success" onClick={onButtonClick}>
+            GitHub
+          </Button>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
 };
 
 export default Navigation;
